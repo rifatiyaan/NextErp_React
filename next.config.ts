567b1d4589aeb5cce100f845b4f439824189db11
rifatiyaan <mdbrifat@gmail.com>
2025-12-18 23:52:00 +0600

    Update API base URL and enhance Next.js config
    Changed the default API base URL to use port 7245. Updated next.config.ts to add Supabase hostname to image domains, expose NEXT_PUBLIC_API_BASE_URL to the environment, and add a rewrite for API routes to proxy requests to the configured API base URL.

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,28 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'example.com',
       },
+
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      }
     ],
   },
+
+
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+
+  // Optional: 
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
